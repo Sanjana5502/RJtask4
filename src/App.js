8a7d305921413cf127ddfeaf761d9baf7dfe8023
Sanjana5502 <sanjanachhipa55@gmail.com>
2023-11-08 17:40:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import { useTable, useSortBy, useFilters, useGlobalFilter, useAsyncDebounce} from "react-table";
+import React, { useState } from "react";
+import {
+  useTable,
+  useSortBy,
+  useFilters,
+  useGlobalFilter,
+  useAsyncDebounce,
+} from "react-table";
 import "./App.css";
 import fakeData from "./MOCK_DATA.json";
-import * as React from "react";
 
 function App() {
+  const [selectedRows, setSelectedRows] = useState(new Set());
   const data = React.useMemo(() => fakeData, []);
   const columns = React.useMemo(
     () => [
       {
-        Header: "ID",
-        accessor: "id",
-      },
-      {
-        Header: "First Name",
-        accessor: "first_name",
-      },
-      {
-        Header: "Last Name",
-        accessor: "last_name",
-      },
-      {
-        Header: "Email",
-        accessor: "email",
-      },
-      {
-        Header: "Gender",
-        accessor: "gender",
-      },
-      {
-        Header: "Phone No.",
-        accessor: "phone_number",
-      },
-      {
-        Header: "Call Date",
-        accessor: "call_date",
-      }
-    ],
-    []
-  );
+        Header: "Customer",
+        accessor: "customer",
+        Cell: ({ row }) => {
+          const isSelected = selectedRows.has(row.id);
 
+          const toggleRowSelection = () => {
+            if (isSelected) {
+              selectedRows.delete(row.id);
+            } else {
+              selectedRows.add(row.id);
+            }
+            setSelectedRows(new Set(selectedRows));
+          };
+
+          return (
+            <div>
+              <input
+                type="checkbox"
+                checked={isSelected}
+                onChange={toggleRowSelection}
+              />
+              {row.values.customer}
+            </div>
+          );
+        },
+      },
+      { Header: "Last Seen", accessor: "lastSeen" },
+      { Header: "No. of Orders", accessor: "orders" },
+      { Header: "Total Spent", accessor: "totalSpent" },
+      { Header: "Latest Purchase", accessor: "latestPurchase" },
+      { Header: "News.", accessor: "news" },
+      { Header: "Segments", accessor: "segments" },
+    ],
+    [selectedRows]
+  );
   const defaultColumn = React.useMemo(
     () => ({
       Filter: DefaultColumnFilter, // Custom filter UI
@@ -46,24 +57,10 @@ function App() {
     []
   );
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    state,
-    setGlobalFilter,
-  } = useTable(
-    { columns, data, defaultColumn },
-    useFilters, // Use column filters
-    useGlobalFilter, // Use global filter
-    useSortBy // Use column sorting
-  );
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, setGlobalFilter} = useTable({ columns, data, defaultColumn }, useFilters,  useGlobalFilter, useSortBy );
 
   const { globalFilter } = state;
 
-  // Define a custom filter UI component
   function DefaultColumnFilter({ column }) {
     const { filterValue, setFilter } = column;
     const [filterInput, setFilterInput] = React.useState(filterValue);
@@ -118,8 +115,11 @@ function App() {
   <tbody {...getTableBodyProps()}>
     {rows.map((row) => {
       prepareRow(row);
+      const isSelected = selectedRows.has(row.id);
       return (
-        <tr {...row.getRowProps()}>
+        <tr {...row.getRowProps()}
+        className={isSelected ? 'selected-row' : ''}
+        >
           {row.cells.map((cell) => (
             <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
           ))
@@ -129,7 +129,6 @@ function App() {
     })}
   </tbody>
 </table>
-
       </div>
     </div>
   );
